0f6b4c8fd366e282c46e30ffe35076e04ca1f31f
Robby Stahl <beefnog@gmail.com>
2016-09-23 15:05:45 -0700

    Minor color change.

--- templates/screenrc.py
+++ templates/screenrc.py
@@ -8,8 +8,8 @@ vbell off
 nethack on
 
 hardstatus alwayslastline 
-# blue hostname, blue pipe, cyan time, blue pipe, cyan window list (current window magenta)
-hardstatus string "%{bk}H: %H | %{ck}%c%{bk} | %{ck}%-w%{mk}%n %t%{ck}%+w "
+# blue hostname, blue pipe, cyan time, blue pipe, cyan window list (current window yellow)
+hardstatus string "%{bk}H: %H | %{ck}%c%{bk} | %{ck}%-w%{yk}%n %t%{ck}%+w "
 
 # default windows
 screen -t L 0 bash
