bb50d9acad9ac2dff7c99a33bfd2c8409baa2c2b
Robby Stahl <beefnog@gmail.com>
2024-10-22 09:46:29 -0700

    updating screenrc template for screen v5.x

--- templates/screenrc.py
+++ templates/screenrc.py
@@ -11,6 +11,10 @@ hardstatus alwayslastline
 # blue hostname, yellow session name, blue pipe, cyan time, blue pipe, cyan window list (current window yellow)
 hardstatus string "%{bk}H: %H,%{yk}%S%{bk} | %{ck}%c%{bk} | %{ck}%-w%{yk}%n %t%{ck}%+w "
 
+# USE THIS ONE for screen v5.x
+# hardstatus string "%{4}H: %H,%{3}%S%{4} | %{6}%c%{4} | %{6}%-w%{3}%n %t%{6}%+w "
+
+
 # default windows
 screen -t L 0 bash
 """
